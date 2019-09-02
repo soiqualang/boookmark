537b7a3c0098dbdba4e6f4c52ef72f6f57920830
soiqualang <soiqualang.chentreu@gmail.com>
2019-09-02 19:09:48 +0700

    u data.php

--- data.php
+++ data.php
@@ -5,7 +5,14 @@ include('funcs.php');
 if(isset($_GET['id'])){
     $id=$_GET['id'];
     //echo '{"content":"Selected: \/root\/dolongstore\/phanmem\/t1.txt","Id":'.$id.'}';
-    
+
+    $nodeid=getElement('fileinfo','nodeid','id',$id,$connection);
+    if($nodeid!='nodeid'){
+        echo $nodeid;
+    }else{
+        echo 'Chua nhap thong tin';
+    }
+
 }else{
     echo 'File khong ton tai';
 }
