ce40562e33f8961dd0b05007c9b3e7da49402bb3
soiqualang <soiqualang.chentreu@gmail.com>
2019-09-02 19:03:02 +0700

    add php connect mysqli

--- data.php
+++ data.php
@@ -1,7 +1,11 @@
 <?php
+include('config.php');
+include('funcs.php');
+//include('libs.php');
 if(isset($_GET['id'])){
     $id=$_GET['id'];
-    echo '{"content":"Selected: \/root\/dolongstore\/phanmem\/t1.txt","Id":'.$id.'}';
+    //echo '{"content":"Selected: \/root\/dolongstore\/phanmem\/t1.txt","Id":'.$id.'}';
+    
 }else{
     echo 'File khong ton tai';
 }
